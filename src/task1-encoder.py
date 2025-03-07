08f58d3b7dd983ee7d560d14724af9b19ed05c28
Fang Liao <anna.fang2@gmail.com>
2025-03-06 21:13:13 -0600

    edit task1 to add comments

--- src/task1-encoder.py
+++ src/task1-encoder.py
@@ -5,6 +5,10 @@ class SentenceEmbeddingModel:
         self.model = SentenceTransformer(model_name)
 
     def encode(self, sentences):
+        """
+        Encodes input sentences into fixed-length embeddings.
+
+        """
         return self.model.encode(sentences)
 
 if __name__ == "__main__":
@@ -12,3 +16,12 @@ if __name__ == "__main__":
     test_sentences = ["Hello world!", "Shop, snap and play to earn free gift cards with Fetch!"]
     embeddings = model.encode(test_sentences)
     print(embeddings)
+    
+"""
+
+Describe choices I had to make:
+    1. Choice of sentence transformer model:
+        I chose all-mpnet-base-v2 because it ranks high in terms of accuracy;
+        Although it's slower than those MiniLM models, it provides more richness with output enbeddings at length of 768 and can intake more length of texts.        
+        
+"""
